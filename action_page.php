302ec886d75299350785e8c88bb4eba2c368b589
SarowarAlam <alamsarowar3@gmail.com>
2020-01-15 13:28:39 +0530

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,9 +2,18 @@
 if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])){
 $to = "yourmail@mailer.com";
 $name = $_POST['name'];
+$subject = "This is a message sent from your Website";
+$msg = $_POST['message'];
+$msg = wordwrap($msg,70);
 $from = $_POST['email'];
-$message = $_POST['message'];
-
-// to be continued
+  
+if (!filter_var($from, FILTER_VALIDATE_EMAIL)) {
+  $emailErr = "Invalid email format";
+  header('Location: contact.php?s="Email Error"');
+  exit();
+}
+$headers = "From:". $from . "\r\n" . $name;
+mail($to,$subject,$msg,$headers);
+ header('Location: contact.php?s="Success"');
 }
 ?>
