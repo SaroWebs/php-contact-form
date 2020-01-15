54ad17cac71773ff5750e95b8af8b66694b72927
SarowarAlam <alamsarowar3@gmail.com>
2020-01-15 13:03:36 +0530

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])){
+$to = "yourmail@mailer.com";
+$name = $_POST['name'];
+$from = $_POST['email'];
+$message = $_POST['message'];
+
+// to be continued
+}
+?>
